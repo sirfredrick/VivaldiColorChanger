0fda1ac0122daf9745472f44f52cb297c09cc227
Unknown <sirfredrick231@gmail.com>
2019-05-17 21:08:37 -0700

    Update Assembly Versions

--- MoveVivaldiFiles/Properties/AssemblyInfo.cs
+++ MoveVivaldiFiles/Properties/AssemblyInfo.cs
@@ -1,10 +1,3 @@
-﻿// Copyright (C) 2018 Jeffrey Tucker
-//
-// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -12,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ColorChangerService")]
+[assembly: AssemblyTitle("Installer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ColorChangerService")]
-[assembly: AssemblyCopyright("Copyright © Jeffrey Tucker 2018")]
+[assembly: AssemblyProduct("Installer")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3b9abb1f-b971-4cb9-bf8a-b445112c0294")]
+[assembly: Guid("cdf7423d-8c99-4bc8-aebc-94905e00bb6c")]
 
 // Version information for an assembly consists of the following four values:
 //
