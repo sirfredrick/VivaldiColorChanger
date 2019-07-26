059bfa78f15398b2526a5aa1eab43e553e7e2ce4
sirfredrick231 <sirfredrick@vivaldi.net>
2019-07-26 16:29:54 -0700

    Update Copyright Notices

--- ColorChangerService/ColorChanger.cs
+++ ColorChangerService/ColorChanger.cs
@@ -1,4 +1,4 @@
-// Copyright (C) 2018 Jeffrey Tucker
+﻿// Copyright (C) 2018, 2019 Jeffrey Tucker
 //
 // This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 //
